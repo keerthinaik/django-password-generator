6500d70cec1bcfd1d8c3fdf4cab7f88bd4933f9b
KEERTHINAIK R <keerthinaik.harika@gmail.com>
2020-05-13 19:36:40 +0530

    added home and password view and html

--- generator/views.py
+++ generator/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render
+import random
 
 # Create your views here.
 def home(request):
-    return HttpResponse("home page view")
+    return render(request,'generator/home.html')
+
+def password(request):
+    
+    characters = list('abcdefghijklmnopqrstuvwxyz')
+    #if length is not found in request default 12 will be selected
+    length = int(request.GET.get('length',12))
+
+    if request.GET.get('uppercase'):
+        characters.extend('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    if request.GET.get('special'):
+        characters.extend('!@#$%^&*()_+-')
+    if request.GET.get('numbers'):
+        characters.extend('0123456789')
+
+    password = ''
+    for x in range(length):
+        password += random.choice(characters)
+    return render(request,'generator/password.html',{'password':password})
